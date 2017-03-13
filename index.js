1a2e2acd8587194a7c60ed6a597369408cdad9ad
Barbara Goss <jaknoclistopadowa@gmail.com>
2017-03-12 22:44:07 -0400

    api app setup

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Author: Barbara Goss
+ * Created: 2017-03-12
+ */
+var express = require('express');
+var app = express();
+
+var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
+
+app.use(webpackMiddleware(webpack(require('./webpack.config'))));
+
+app.use(express.static('public'));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/flimsy');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use('/api/films', require('./api/films'));
+app.use('/api/users', require('./api/users'));
+app.use('/api/venues', require('./api/venues'));
+app.use('/api/screenings', require('./api/screenings'));
+app.use('/api/media', require('./api/media'));
+
+app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.listen(8080);
